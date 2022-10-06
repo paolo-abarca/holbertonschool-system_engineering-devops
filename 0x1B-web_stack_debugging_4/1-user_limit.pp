ba701aaa35869b9b775d1cf1a6b42124eda6f9f9
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-10-06 02:54:28 -0500

    Adding solution for the task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# script that fixes holberton error
+
+exec { 'error-holberton':
+  command => 'sudo sed -i s/hard nofile/hard nofile 50000/ /etc/security/limits.conf; sudo sed -i s/soft nofile/soft nofile 40000 /etc/security/limits.conf',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
